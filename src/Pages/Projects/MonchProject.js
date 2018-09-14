06598e6961909260bdd3a0b72ffae4445860d64c
Eva Barrett <Evabarrett23@Gmail.com>
2018-09-14 15:43:24 -0700

    fixed sub components

---
+++ src/Pages/Projects/MonchProject.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class MonchProject extends Component {
+  constructor(props) {
+    super(props)
+
+  }
+
+  render() {
+    return (
+        <h2>This is the Monch Project component</h2>
+    )
+  }
+}
+
+export default MonchProject;
---
+++ src/Pages/Projects/Moodsic.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Moodsic extends Component {
+  constructor(props) {
+    super(props)
+
+  }
+
+  render() {
+    return (
+        <h2>This is the Moodsic component</h2>
+    )
+  }
+}
+
+export default Moodsic;
