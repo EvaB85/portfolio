fedebcdc37cbe8a5599666cd8f9544888311362e
Eva Barrett <Evabarrett23@Gmail.com>
2018-09-15 18:58:54 -0700

    fixed profile img styling

--- src/Pages/BottomBar/BottomBarHome.js
+++ src/Pages/BottomBar/BottomBarHome.js
@@ -12,11 +12,7 @@ class BottomBarHome extends Component {
         <p>Sleep. Meow go back to sleep owner brings food and water
           tries to pet on head, so scratch get sprayed by water because
           bad cat scratch the furniture so attack the child but be a
-          nyan cat, feel great about it, be annoying 24/7 poop rainbows
-          in litter box all day. Annoy kitten brother with poking try to
-          jump onto window and fall while scratching at wall for freak
-          human out make funny noise mow mow mow mow mow mow success now
-          attack human poop in the plant pot taco cat backwards spells
+          nyan cat, w mow mow mow mow success nowattack human poop in the
           taco cat litter kitter kitty litty little kitten big roar roar
           feed me all of a sudden cat goes crazy. Lick arm hair. Really
           likes hummus.
