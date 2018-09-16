45de91a10ca48ac4bffd85deb16ec7f190f8d380
Eva Barrett <Evabarrett23@Gmail.com>
2018-09-15 18:19:07 -0700

    added overflow-x scroll and icon arrows

--- src/Pages/BottomBar/BottomBarProjects.js
+++ src/Pages/BottomBar/BottomBarProjects.js
@@ -9,10 +9,17 @@ class BottomBarProjects extends Component {
 
   render() {
     return (
-      <div className="bottom-bar-projects">
-        <Link className="projects-box projects-box--fitness-trails" to="/projects/fitness-trails">FITNESS TRAILS APP</Link>
-        <Link className="projects-box projects-box--moodsic" to="/projects/moodsic">MOODSIC</Link>
-        <Link className="projects-box projects-box--monch-project" to="/projects/monch-project">MONCH! MONCH! MONCH!</Link>
+      <div className='bottom-bar-projects--outer'>
+        <div className="bottom-bar-projects">
+          <Link className="projects-box projects-box--fitness-trails" to="/projects/fitness-trails">FITNESS TRAILS APP</Link>
+          <Link className="projects-box projects-box--moodsic" to="/projects/moodsic">MOODSIC</Link>
+          <Link className="projects-box projects-box--monch-project" to="/projects/monch-project">MONCH! MONCH! MONCH!</Link>
+          {/* TESTING */}
+          <Link className="projects-box projects-box--monch-project" to="/projects/monch-project">MONCH! MONCH! MONCH!</Link>
+          <Link className="projects-box projects-box--monch-project" to="/projects/monch-project">MONCH! MONCH! MONCH!</Link>
+        </div>
+        <i className="arrow arrow--left fas fa-chevron-left"></i>
+        <i className="arrow arrow--right fas fa-chevron-right"></i>
       </div>
     )
   }
