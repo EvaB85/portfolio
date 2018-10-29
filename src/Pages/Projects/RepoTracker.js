18dcc42c11e37c23106eb18565a172527c147dcd
Eva Barrett <Evabarrett23@Gmail.com>
2018-10-29 15:25:03 -0700

    changed naming convention projects pg

--- src/Pages/Projects/RepoTracker.js
+++ src/Pages/Projects/RepoTracker.js
@@ -11,7 +11,12 @@ class RepoTracker extends Component {
       <div>
         <a className='live-site-link' href='https://gitman.herokuapp.com/' target='_blank' rel='noopener noreferrer'>Live Site</a>
         <a className='github-site-link' href='https://github.com/carverme/project_four2' target='_blank' rel='noopener noreferrer'>GitHub</a>
-        <h2>RepoTracker gives users the ability to track their time spent on their coding repositories. Be able to monitor while you study, work on projects, or just for fun.  </h2>
+        <div className='repo-tracker-box'>
+          <h2>Description:</h2>
+          <p>RepoTracker gives users the ability to track their time spent on their coding repositories. Be able to monitor while you study, work on projects, or just for fun.  </p>
+          <h2>Technologies Used:</h2>
+          <p>INSERT TECHS.........</p>
+        </div>
       </div>
     )
   }
